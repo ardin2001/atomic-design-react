e02be418bd0723176a95957f1758f4c5ce4b975f
ardinnugraha2001@gmail.com <ardinnugraha2001@gmail.com>
2023-06-28 10:54:27 +0700

    react router

--- src/components/elements/input/input.jsx
+++ src/components/elements/input/input.jsx
@@ -1,9 +1,9 @@
 const Input = props =>{
-    const {type,placeholder} = props
+    const {type,placeholder,name,id} = props
     return(
         <input type={type}
         className="text-sm border rounded w-full p-2 text-slate-700 placeholder:opacity-50"
-        placeholder={placeholder} />
+        placeholder={placeholder} name={name} id={id} />
     )
 }
 
