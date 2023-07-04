48004166162ee4d34de5c14f806816a330bc763c
ardinnugraha2001@gmail.com <ardinnugraha2001@gmail.com>
2023-07-04 08:40:32 +0700

    response error login

--- src/components/fragments/formLogin.jsx
+++ src/components/fragments/formLogin.jsx
@@ -1,19 +1,30 @@
 import InputForm from "../elements/input";
-import {Button} from "../elements/button";
-import { forwardRef } from "react";
-import { useNavigate } from "react-router-dom";
+import { Button } from "../elements/button";
+import { forwardRef, useEffect, useState } from "react";
+import { json, useNavigate } from "react-router-dom";
 import { loginUser } from "../../utils/api";
 
 const FormLogin = forwardRef((props, ref) => {
+  const [error, setError] = useState("");
   const navigate = useNavigate();
   const onSubmitHandler = (event) => {
     event.preventDefault();
+
     async function Login(username, password) {
-      let response = await loginUser({ username, password });
-      if (response.status == 200) {
+      try {
+        let response = await loginUser({ username, password });
+        console.log(response)
+        if(!response.ok){
+          response = await response.json();
+          throw new Error(response)
+        }
+
         response = await response.json();
-        localStorage.setItem("token",response.token)
+        localStorage.setItem("token", response.token);
         navigate("/products-api");
+      } catch (err) {
+        setError(err.message)
+        console.log(err.message)
       }
     }
     Login(event.target.username.value, event.target.password.value);
@@ -30,6 +41,7 @@ const FormLogin = forwardRef((props, ref) => {
       <Button classname="bg-blue-600 w-full" type="submit">
         Login
       </Button>
+      <p className="font-medium text-red-600 text-center">{error}</p>
     </form>
   );
 });
